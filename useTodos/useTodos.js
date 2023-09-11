737d918291b184a2a00d47775e2c42f21ccaa1f9
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-09-11 09:52:30 -0300

    improving structure

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import { useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { todoReducer } from './todoReducer';
 
 export const useTodos = (initialState = []) => {
     const [todos, dispatch] = useReducer(todoReducer, initialState);
